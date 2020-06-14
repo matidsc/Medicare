4de420c9fd64ed7d60f76abb205ba24911321a21
alexisUTU <alexis333utu@gmail.com>
2020-06-14 20:19:16 -0300

    .

--- Nuevo/Vista/My Project/Application.Designer.vb
+++ Nuevo/Vista/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Vista.RegistrarMedico
+            Me.MainForm = Global.Vista.RegistrarSintomas
         End Sub
     End Class
 End Namespace
